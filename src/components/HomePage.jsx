c0bb05ddbd97ba6b46fe7057b453a98f09ae21f2
Abraham Serena <abraham.serena.lk@gmail.com>
2022-01-24 22:30:26 -0600

    Fetched Global Crypto Stats on Home Page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
 const HomePage = () => {
+    const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+
+    console.log(data);
+
+    if (isFetching) return 'Loading...';
+
     return (
         <>
             <div className="main-content flex flex-col flex-grow p-4">
@@ -11,23 +20,23 @@ const HomePage = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
                         <div className="flex flex-col justify-center border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
                             <h2>Total Cryptocurrencies</h2>
-                            <span>5</span>
+                            <span>{globalStats.total}</span>
                         </div>
                         <div className="flex flex-col justify-center border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
                             <h2>Total Exchanges</h2>
-                            <span>5</span>
+                            <span>{millify(globalStats.totalExchanges)}</span>
                         </div>
                         <div className="flex flex-col justify-center border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
                             <h2>Total Market Cap</h2>
-                            <span>5</span>
+                            <span>{millify(globalStats.totalMarketCap)}</span>
                         </div>
                         <div className="flex flex-col justify-center border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
                             <h2>Total 24h Volume</h2>
-                            <span>5</span>
+                            <span>{millify(globalStats.total24hVolume)}</span>
                         </div>
                         <div className="flex flex-col justify-center border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
                             <h2>Total Markets</h2>
-                            <span>5</span>
+                            <span>{millify(globalStats.totalMarkets)}</span>
                         </div>
                     </div>
                 </div>
