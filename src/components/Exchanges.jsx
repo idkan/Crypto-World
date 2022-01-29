afbe9dfc4dc48bb703fcf617a205ee556dd1b347
Abraham Serena <abraham.serena.lk@gmail.com>
2022-01-29 01:46:39 -0600

    added todoo

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -5,6 +5,7 @@ const Exchanges = () => {
         <div className="main-content flex flex-col flex-grow p-4">
             <h1 className="font-bold text-2xl text-gray-700">Exchanges</h1>
             <div className="flex flex-col flex-grow border-4 border-gray-400 border-dashed bg-white rounded mt-4" ></div>
+            {/* TODO: Change this section */}
         </div>
     );
 };
